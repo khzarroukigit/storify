2e049eb7cb8cff67a4326f99dc118e01f7b82152
khzarroukigit <khzarroukigit@gmail.com>
2021-12-17 11:14:07 +0100

    working with layouts

--- resources/views/home.blade copy.php
+++ resources/views/home.blade copy.php
@@ -1,3 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
 <h1>Welcome to About page !!!</h1>
 
 {{ $var1 }}
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
 <h1>Welcome to home page !!!</h1>
 
 {{ $var1 }}
+
+@endsection
